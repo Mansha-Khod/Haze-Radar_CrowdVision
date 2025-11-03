6b67cc7771910c741682d1f55f6a45a26689abee
Mansha-Khod <manshakhod@gmail.com>
2025-11-03 23:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -354,10 +354,11 @@ def home():
         },
         'status': 'active'
     })
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 8080))  # ✅ dynamic port for Streamlit
-    app.run(debug=False, host='0.0.0.0', port=port)
+import os
+
+port = int(os.environ.get("PORT", 7860))
+app.run(debug=False, host='0.0.0.0', port=port)
+
 
 
 
