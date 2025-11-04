2c56b657e4a4165878fe8363859ee6fb658fa2cc
Mansha-Khod <manshakhod@gmail.com>
2025-11-04 17:36:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import base64
+
 import os
 import gdown
 import torch
@@ -176,6 +178,7 @@ def home():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
